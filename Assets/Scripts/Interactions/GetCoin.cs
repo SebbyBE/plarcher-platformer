eedd3f9ce012668dc0a5611a1c4ab09c819bcdd5
SebbyBe <sebastien.place@yahoo.fr>
2017-11-15 19:59:23 +0100

    add coins to player and prefab coin, player can grab coins

---
+++ Assets/Scripts/Interactions/GetCoin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCoin : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.tag == "Coin") {
+			Player.INSTANCE.coins++;
+			Destroy (col.transform.parent.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Movements/KeyboardMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour {
+
+	[SerializeField]
+	int speed = 1;
+
+	void Awake() {
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		KinematicMovement ();
+	}
+
+
+
+	void KinematicMovement() {
+		Vector3 velocity = GetAxisVector ();
+		velocity *= speed * Time.deltaTime;
+		transform.position += velocity;
+	}
+
+	Vector3 GetAxisVector() {
+		float hDirection = Input.GetAxisRaw ("Horizontal");
+		float vDirection = Input.GetAxisRaw ("Vertical");
+		return new Vector3 (hDirection, vDirection, 0);
+	}
+
+}
