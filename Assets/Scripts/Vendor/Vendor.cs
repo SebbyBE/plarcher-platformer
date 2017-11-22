c45b361649bae366b9766f5d2d97f48605b880dd
kyky <kyrilltircher14@hotmail.com>
2017-11-22 16:28:12 +0100

    UI vendor

---
+++ Assets/Scripts/Vendor/Vendor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vendor : MonoBehaviour {
+
+	private bool canInteract = false;
+	public GameObject panel;
+
+	void OnTriggerEnter2D(Collider2D vendor){
+		print ("coll");
+		if (vendor.tag == "Player") {
+			canInteract = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D vendor){
+		if (vendor.tag == "Player") {
+			canInteract = false;
+			panel.SetActive (false);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire3") && canInteract) {
+			panel.SetActive (!panel.activeSelf);
+		}
+	}
+}
