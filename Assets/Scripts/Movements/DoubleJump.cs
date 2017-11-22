cfaef46d494f7ae9bac6a8e5efe7444615a14938
SebbyBE <sebastien.place@yahoo.fr>
2017-11-22 13:10:21 +0100

    avant midi

---
+++ Assets/Scripts/Movements/DoubleJump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJump : MonoBehaviour {
+	bool canDoubleJump = false;
+	bool keyIsPressed = false;
+
+
+	void Awake(){
+		Player.INSTANCE.gameObject.GetComponent<AddJump> ().enabled = false;
+	}
+
+	void Update () {
+		if(Input.GetButton("Jump")){
+			if (Player.INSTANCE.isGrounded) {
+				Player.INSTANCE.rigidBody.velocity = new Vector2 (Player.INSTANCE.rigidBody.velocity.x, 0);
+				Player.INSTANCE.rigidBody.AddForce (Vector3.up * Player.INSTANCE.jumpStrength, ForceMode2D.Impulse);
+				canDoubleJump = true;
+				keyIsPressed = true;
+			} else {
+				if (canDoubleJump && !keyIsPressed) {
+					canDoubleJump = false;
+					Player.INSTANCE.rigidBody.velocity = new Vector2(Player.INSTANCE.rigidBody.velocity.x,0);
+					Player.INSTANCE.rigidBody.AddForce (Vector3.up * Player.INSTANCE.jumpStrength, ForceMode2D.Impulse);
+				}
+			}
+		}
+		if(Input.GetButtonUp("Jump")){
+			keyIsPressed = false;
+		}
+	}
+}
