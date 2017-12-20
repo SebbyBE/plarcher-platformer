69f1d46542361f02681210b77b87540070b2afff
SebbyBe <sebastien.place@yahoo.fr>
2017-12-20 17:27:21 +0100

    Working menu

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+	public Transform pauseCanvas;
+
+	void OnEnable(){
+		Time.timeScale = 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Pause ();
+		}	
+	}
+
+	public void Pause(){
+		if (pauseCanvas.gameObject.activeInHierarchy == false) {
+			pauseCanvas.gameObject.SetActive (true);
+			Time.timeScale = 0;
+		} else {
+			pauseCanvas.gameObject.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+
+	public void Exit(){
+	
+	}
+
+	public void Restart(){
+	
+	}
+}
