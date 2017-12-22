ab957cab239744e0c7ff4aed7b88c06e30b8c868
SebbyBe <sebastien.place@yahoo.fr>
2017-12-22 12:25:12 +0100

    oupsyyy

--- Assets/Scripts/Movements/RightComponent.cs
+++ Assets/Scripts/Movements/RightComponent.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RightComponent : MonoBehaviour {
 	void Update(){
 		float hDirection = Input.GetAxisRaw ("Horizontal");
-		Player.INSTANCE.anim.SetFloat ("Speed", Mathf.Abs (hDirection));
+		//Player.INSTANCE.anim.SetFloat ("Speed", Mathf.Abs (hDirection));
 		if (!Player.INSTANCE.isInvincible && hDirection > 0) {
 			Vector3 velocity = new Vector3 (hDirection, 0, 0);
 			velocity *=  Player.INSTANCE.speed * Time.deltaTime;
